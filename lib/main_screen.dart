e8cad12175a8ac529f7679ac904cefd2f33ace1d
unknown <ensafelazzeh1999@gmail.com>
2024-06-09 15:01:07 +0300

    star with the design

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,5 +1,6 @@
 
   import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class MainScreen extends StatefulWidget {
     const MainScreen({super.key});
@@ -9,6 +10,10 @@ class MainScreen extends StatefulWidget {
   }
   
   class _MainScreenState extends State<MainScreen> {
+      final DateTime now = DateTime.now();
+        final DateFormat formatter = DateFormat('dd MMMM yyyy');
+      
+           
     @override
     Widget build(BuildContext context) {
       return Scaffold( backgroundColor: Colors.grey[100],
@@ -17,11 +22,17 @@ class MainScreen extends StatefulWidget {
           padding: const EdgeInsets.all(40.0),
           child: Column(children: 
           [Row( mainAxisAlignment: MainAxisAlignment.spaceBetween ,children: [
-            Icon(Icons.menu),
-            Text("Date"),
-            Icon(Icons.search)
+            Icon(Icons.menu , size: 35,),
+            // Text("Today,${DateTime.now().day} ${DateTime.now().month} ${DateTime.now().year} " , style: TextStyle(color: Colors.grey , fontWeight: FontWeight.w600, fontSize: 15),),
+            Text("Today,${formatter.format(now)}" , style: TextStyle(color: Colors.grey , fontWeight: FontWeight.w600, fontSize: 15),),
+            Icon(Icons.search , size: 35,)
           
-          ],)],),
+          ],),
+          Row(
+            children: [
+              Image.network("https://img.freepik.com/premium-psd/png-pizza-fast-food-image-transparent-background_1022554-37.jpg", width: 150,),
+            ],
+          )],),
         ),
       )
       
